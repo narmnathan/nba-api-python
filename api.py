30236f5d238027a0f860cf5f9c1dd856a44076a4
narmnathan <narmnathan@gmail.com>
2023-02-17 19:16:31 -0500

    debugged and functional

--- api.py
+++ api.py
@@ -5,18 +5,29 @@ import os
 
 # Find API player ID with full name.
 def id_find(player_name):
-    id = players.find_players_by_full_name(player_name)[0]['id']
-    return id
+    try:
+        players.find_players_by_full_name(player_name)[0]['id']
+    except:
+        print("Player not found: " + player_name + "\n Please reload.")
+    else:
+        id = players.find_players_by_full_name(player_name)[0]['id']
+        return id
 
 
 def game_log(player_name):
-    log = playergamelog.PlayerGameLog(player_id=id_find(player_name)).get_data_frames()[0]
-    filename = player_name + '.csv'
-    pathname = '~Desktop/sinix-model/' + filename
-    check = os.path.isfile(pathname)
-    while not check:
-        log.to_csv(filename, index=False)
-        return log
+    try:
+        players.find_players_by_full_name(player_name)[0]['id']
+    except:
+        print("Player not found: " + player_name + "\nPlease retry.")
+    else:
+        log = playergamelog.PlayerGameLog(player_id=id_find(player_name)).get_data_frames()[0]
+        filename = player_name + '.csv'
+        pathname = '~Desktop/sinix-model/' + filename
+        check = os.path.isfile(pathname)
+        while not check:
+            log.to_csv(filename, index=False)
+            print("CSV load successful for " + player_name)
+            return log
 
 
 
--- csv_load.py
+++ csv_load.py
@@ -1,11 +1,12 @@
 import api
 
 # Store player names to retrieve respective CSVs
-player_names = []
+# player_names = []
 
 # Create function to load player_names with input
+input = input("Enter player names to retrieve with full names separated by commas. \ne.g.: Anthony Edwards, LeBron James, Joel Embiid\n")
+player_names = input.split(", ")
 # load = input("Enter desired player full names (followed by enter), then type 'load' to load CSVs.\n")
-
 for name in player_names:
     api.game_log(name)
 
