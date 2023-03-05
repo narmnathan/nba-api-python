8fe43ac5078fb005ebad8514f4b014906af03397
narmnathan <narmnathan@gmail.com>
2023-03-05 12:48:44 -0500

    update

--- csv_load.py
+++ csv_load.py
@@ -7,13 +7,13 @@ import api, testing
 player_input = input("Enter player names to retrieve with full names separated by commas. \ne.g.: Anthony Edwards, LeBron James, Joel Embiid\n")
 player_names = player_input.split(", ")
 for name in player_names:
-    testing.player_game_log(name)
+    api.player_game_log(name)
 
 team_input = input("Enter team abbreviations to retrieve separated by commas. \ne.g.: BKN, ATL, TOR\n")
 team_abbrs = team_input.split(", ")
 
 for team in team_abbrs:
-    testing.team_game_log(team)
+    api.team_game_log(team)
 
 
 
